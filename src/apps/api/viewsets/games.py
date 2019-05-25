699d3d5ce0d1014e2d4d1d633bad7d4bb1ea5a45
Eugene <79889501003@yandex.ru>
2019-05-25 10:21:52 +0300

    Games:
    * added new endpoint for the game data representation

--- src/apps/api/viewsets/games.py
+++ src/apps/api/viewsets/games.py
@@ -2,7 +2,8 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from apps.api.serializers import GameSerializer, GameExtendedSerializer
+from apps.api.serializers import GameSerializer, GameExtendedSerializer, \
+    LocationSerializer, TransitionSerializer
 from apps.main.models import Game
 
 
@@ -16,3 +17,15 @@ class GameViewSet(ModelViewSet):
         instance = self.get_object()
         serializer = self.get_serializer(instance=instance)
         return Response(serializer.data)
+
+    @action(methods=['get'], detail=True)
+    def full(self, *args, **kwargs):
+        instance = self.get_object()
+        transitions = instance.transitions.all()
+        locations = instance.locations.all()
+        l_serializer = LocationSerializer(locations, many=True)
+        t_serializer = TransitionSerializer(transitions, many=True)
+        return Response({
+            'locations': l_serializer.data,
+            'transitions': t_serializer.data
+        })
--- src/apps/api/viewsets/locations.py
+++ src/apps/api/viewsets/locations.py
@@ -1,24 +1,11 @@
-from rest_framework.decorators import action
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from apps.api.filters import LocationFilter
-from apps.api.serializers import LocationSerializer, TransitionSerializer
-from apps.main.models import Location, Transition
+from apps.api.serializers import LocationSerializer
+from apps.main.models import Location
 
 
 class LocationViewSet(ModelViewSet):
     serializer_class = LocationSerializer
     queryset = Location.objects.all()
     filter_class = LocationFilter
-
-    @action(methods=['get'], detail=False)
-    def full(self, *args, **kwargs):
-        locations = Location.objects.all()
-        transitions = Transition.objects.all()
-        l_serializer = LocationSerializer(locations, many=True)
-        t_serializer = TransitionSerializer(transitions, many=True)
-        return Response({
-            'locations': l_serializer.data,
-            'transitions': t_serializer.data
-        })
